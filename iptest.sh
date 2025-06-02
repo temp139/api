446e2e5a9de8b37390cae1c9c83ae0800c051058
temp139 <960674672@qq.com>
2025-06-02 21:58:49 +0800

    Update iptest.sh

--- iptest.sh
+++ iptest.sh
@@ -1,3 +1,3 @@
    #!/bin/bash
 
-   ./iptest -file="ip.txt"  -speedtest=3 -speedlimit=8 -url="speed.cloudflare.com/__down?bytes=50000000" -max=100 -outfile="ip.csv"
+   ./iptest -file="ip.txt"  -speedtest=3 -speedlimit=100 -url="speed.cloudflare.com/__down?bytes=50000000" -max=100 -outfile="ip.csv"
